571715db4dccef39e89a521aa99b2fc491b0fae0
myj4513 <yejungod@gmail.com>
2021-07-27 13:40:32 +0900

    day4

--- src/gymSuit/GymSuit.java
+++ src/gymSuit/GymSuit.java
@@ -1,43 +1,37 @@
 package gymSuit;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GymSuit {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(5, new int[]{2,3,4}, new int[]{3,4,5}));
+        System.out.println(s.solution(5, new int[]{2,4}, new int[]{1,3,5}));
     }
 }
 
 class Solution{
     public int solution(int n, int[] lost, int[] reserve) {
-        List<Integer> lostList = new ArrayList<>();
-        for(int num : lost) lostList.add(num);
-        List<Integer> reserveList = new ArrayList<>();
-        for(int num : reserve) reserveList.add(num);
-
-        for(int i=0;i<lostList.size();i++){
-            for(int j=0;j<reserveList.size();j++){
-                if(lostList.get(i)==reserveList.get(j)){
-                    lostList.set(i,0);
-                    reserveList.set(j,0);
+        int[] people = new int[n];
+        for(int r : reserve){
+            people[r-1]++;
+        }
+        for(int l : lost){
+            people[l-1]--;
+        }
+        for(int i=0;i<people.length;i++){
+            if(people[i]==-1){
+                if(i-1>0 && people[i-1]==1){
+                    people[i]++;
+                    people[i-1]--;
+                } else if(i+1<people.length && people[i+1]==1){
+                    people[i]++;
+                    people[i+1]--;
                 }
             }
         }
-
-        for(int i=0;i<reserveList.size();i++){
-            if(reserveList.get(i)!= 0 && lostList.contains(reserveList.get(i)-1)){
-                lostList.set(lostList.indexOf(reserveList.get(i)-1), 0);
-            } else if(reserveList.get(i)!= 0 && lostList.contains(reserveList.get(i)+1)){
-                lostList.set(lostList.indexOf(reserveList.get(i)+1), 0);
+        for(int p : people){
+            if(p==-1){
+                n--;
             }
         }
-
-        int count = 0;
-        for(int l : lostList)
-            if(l!=0) count++;
-
-        return n-count;
+        return n;
     }
 }
