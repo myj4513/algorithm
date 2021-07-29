b811ceac7e8adf54683eef2ff3330f58d9e99e7a
myj4513 <yejungod@gmail.com>
2021-07-29 13:39:25 +0900

    day 6

---
+++ src/dayOf2016/DayOf2016.java
@@ -0,0 +1,20 @@
+package dayOf2016;
+
+import java.util.Calendar;
+
+public class DayOf2016 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(5,24));
+    }
+}
+
+class Solution {
+    String[] dayOfWeek = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+    public String solution(int a, int b){
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016,a-1,b);
+
+        return dayOfWeek[cal.get(Calendar.DAY_OF_WEEK)-1];
+    }
+}
---
+++ src/twoNums/TwoNums.java
@@ -0,0 +1,31 @@
+package twoNums;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoNums {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] answer = s.solution(new int[]{10,3,3,2,2,1,});
+        System.out.println(Arrays.toString(answer));
+    }
+}
+
+class Solution{
+    public int[] solution(int[] numbers){
+
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<numbers.length-1;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        int[] answer = new int[set.size()];
+        Object[] temp = set.toArray();
+        for(int i=0;i<set.size();i++){
+            answer[i] = (int)temp[i];
+        }
+        return set.stream().sorted().mapToInt(i->i.intValue()).toArray();
+    }
+}
