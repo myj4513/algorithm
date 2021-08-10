2ef8c43546be1ca8d6755e4d32a39ab97f4dd530
myj4513 <yejungod@gmail.com>
2021-08-10 19:26:29 +0900

    day19

---
+++ src/level2/makePrime/MakePrime.java
@@ -0,0 +1,58 @@
+package level2.makePrime;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MakePrime {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("999999999"));
+        System.out.println(s.isPrime(9));
+    }
+}
+
+class Solution {
+    String num;
+    Set<Integer> set = new HashSet<>();
+    public int solution(String numbers) {
+        num = numbers;
+        int answer = 0;
+
+        for(int i=1;i<=numbers.length();i++){
+            permutation(0, i, new boolean[numbers.length()], "");
+        }
+        System.out.println(set);
+        for(int num : set){
+            if(isPrime(num))
+                answer++;
+        }
+        return answer;
+    }
+
+    public void permutation(int count, int max, boolean[] check, String s){
+        if(count==max){
+            set.add(Integer.parseInt(s));
+            return;
+        }
+        for(int i=0;i<num.length();i++){
+            if(check[i]==false){
+                char c = num.charAt(i);
+                check[i]=true;
+                permutation(count+1, max, check, s+c);
+                check[i]=false;
+            }
+        }
+    }
+
+    public boolean isPrime(int n){
+        if(n<=1) return false;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/level2/rankSearch/RankSearch.java
+++ src/level2/rankSearch/RankSearch.java
@@ -1,39 +1,78 @@
 package level2.rankSearch;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class RankSearch {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] result = s.solution(new String[]{"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"},
-                new String[]{"- and - and - and - 100"});
+        int[] result = s.solution(new String[]{"java backend junior pizza 150"},
+                new String[]{"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"});
         System.out.println(Arrays.toString(result));
     }
 }
 
 class Solution {
-    List<ArrayList<String>> list = new ArrayList<>();
-    String[][] q = new String[][]{{"java", "python", "cpp", "-"},
-            {"frontend", "backend", "-"},
-            {"junior", "senior", "-"},
-            {"pizza", "chicken", "-"}};
+    Map<String, List<Integer>> map = new HashMap<>();
+    String[][] str = {{"-","java","cpp","python"}, {"-","backend","frontend"},{"-","junior","senior"},{"-","chicken","pizza"}};
+    List<String> list = new ArrayList<>();
     public int[] solution(String[] info, String[] query) {
-        comb(0, q.length, new ArrayList<String>());
-        System.out.println(list);
-        return new int[]{};
+        int answer = 0;
+        comb(0, 4, "");
+
+        for(int i=0;i<list.size();i++){
+            map.put(list.get(i), new ArrayList<Integer>());
+        }
+
+        for(int i=0;i< info.length;i++){
+            String[] word = info[i].split(" ");
+            List<String> list2 = new ArrayList<String>();
+            comb2(0, 4, "", word, list2);
+            for(int j=0;j<list2.size();j++){
+                String s = list2.get(j);
+                List<Integer> l = map.get(s);
+                l.add(Integer.parseInt(word[4]));
+                map.put(s, l);
+            }
+        }
+        int[] result = new int[query.length];
+        for(int i=0;i< query.length;i++){
+            String[] word = query[i].split(" ");
+            String s = word[0]+word[2]+word[4]+word[6];
+            List<Integer> integers = map.get(s);
+            integers.sort(Comparator.naturalOrder());
+            int idx = Collections.binarySearch(integers, Integer.parseInt(word[7]));
+            if(idx<0){
+                idx = -(idx+1);
+            } else{
+                if(idx-1>=0){
+                    while(integers.get(idx-1)== integers.get(idx)){
+                        idx--;
+                    }
+                }
+            }
+            answer = integers.size() - idx;
+            result[i] = answer;
+        }
+
+        return result;
     }
 
-    public void comb(int index, int maxIndex, ArrayList<String> tmp){
-        if(index>=maxIndex){
-            list.add(new ArrayList<>(tmp));
+    public void comb2(int count, int maxCount, String s, String[] word, List<String> list2){
+        if(count==maxCount){
+            list2.add(s);
             return;
         }
-        for(int i=0;i<q[index].length;i++){
-            tmp.add(q[index][i]);
-            comb(index + 1, maxIndex, tmp);
-            tmp.remove(q[index][i]);
+        comb2(count+1, maxCount, s+"-", word, list2);
+        comb2(count+1, maxCount, s+word[count], word, list2);
+    }
+
+    public void comb(int count, int maxCount, String s){
+        if(count==maxCount){
+            list.add(s);
+            return;
+        }
+        for(int i=0;i<str[count].length;i++){
+            comb(count+1, maxCount, s+str[count][i]);
         }
     }
 }
