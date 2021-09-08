26bd6d6392db7ac531e3508fa14697eb7deaef0a
myj4513 <yejungod@gmail.com>
2021-09-08 23:58:14 +0900

    day36

---
+++ src/level2/dictionary/Dictionary.java
@@ -0,0 +1,25 @@
+package level2.dictionary;
+
+public class Dictionary {
+    public static void main(String[] args) {
+        Solution s= new Solution();
+        int answer = s.solution("AAAE");
+
+        System.out.println("answer = " + answer);
+    }
+}
+
+class Solution {
+    String[] letter = {"", "A", "E", "I", "O", "U"};
+
+    public int solution(String word) {
+        int answer = 0;
+        String s;
+
+        for(int i=word.length();i>=0;i++){
+
+        }
+
+        return -1;
+    }
+}
