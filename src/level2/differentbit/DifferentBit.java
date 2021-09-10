bba5a1c42edb4c1701c598f68a0775532488fd37
myj4513 <yejungod@gmail.com>
2021-09-11 00:06:36 +0900

    different bit2

--- src/level2/differentbit/DifferentBit.java
+++ src/level2/differentbit/DifferentBit.java
@@ -5,36 +5,10 @@ import java.util.Arrays;
 public class DifferentBit {
     public static void main(String[] args) {
         Solution s = new Solution();
-        long[] answer = s.solution(new long[]{2, 31});
+        long[] answer = s.solution(new long[]{2, 7});
         System.out.println("answer = " + Arrays.toString(answer));
     }
 }
-
-/*class Solution {
-    public long[] solution(long[] numbers) {
-        long[] answer = new long[numbers.length];
-
-        for(int i=0;i<numbers.length;i++){
-            long num = numbers[i];
-            while(true){
-                num++;
-                String binary = Long.toString(numbers[i]^num, 2);
-                int count = 0;
-                for(int j=0;j<binary.length();j++){
-                    if(binary.charAt(j)=='1')
-                        count++;
-                    if(count>2)
-                        break;
-                }
-                if(count<=2)
-                    break;
-            }
-            answer[i] = num;
-        }
-
-        return answer;
-    }
-}*/
 
 class Solution {
     public long[] solution(long[] numbers) {
@@ -45,16 +19,21 @@ class Solution {
                 answer[i] = 2;
             }
             else{
+                String s = Long.toString(numbers[i], 2);
                 int index0 = Long.toString(numbers[i], 2).lastIndexOf("0");
                 if(index0==-1){
-                    String s = Long.toString(numbers[i], 2).substring(1);
+                    s = s.substring(1);
                     answer[i] = Long.parseLong("10"+s, 2);
 
                 }
+                else if(index0 == s.length()-1){
+                    answer[i] = ++numbers[i];
+                }
                 else{
-                    char[] chars = Long.toString(numbers[i], 2).toCharArray();
+                    char[] chars = s.toCharArray();
                     chars[index0] = '1';
-                    answer[i] = Long.parseLong(chars.toString(),2);
+                    chars[index0+1] = '0';
+                    answer[i] = Long.parseLong(String.valueOf(chars),2);
                 }
             }
 
