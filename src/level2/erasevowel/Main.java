320dc5efa69426849ae3cf0a65ec6bb1b8a84c5b
myj4513 <yejungod@gmail.com>
2021-09-23 23:49:01 +0900

    erasevowel

---
+++ src/level2/erasevowel/Main.java
@@ -0,0 +1,23 @@
+package level2.erasevowel;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
+        int n = Integer.parseInt(scanner.nextLine());
+        for(int j=0;j<n;j++){
+            int answer = 0;
+            String s = scanner.nextLine();
+            for(int i=0;i<s.length();i++){
+                if(vowels.contains(s.charAt(i))){
+                    answer += s.length()-i-1;
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+}
