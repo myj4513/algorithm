b70c8667d8e61c0985384afcf778989c75cb29a8
myj4513 <yejungod@gmail.com>
2021-12-25 16:14:41 +0900

    백준 2167번

---
+++ src/baekjoon/p2167/Main.java
@@ -0,0 +1,40 @@
+package baekjoon.p2167;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N+1][M+1];
+
+        for(int i=1;i<=N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=M;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+        while (K-- > 0) {
+            int sum = 0;
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for(int a=i;a<=x;a++){
+                for(int b=j;b<=y;b++){
+                    sum += arr[a][b];
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
