a688240ef4006fbffbb2dac559ed886fea83e906
myj4513 <yejungod@gmail.com>
2022-01-04 14:19:05 +0900

    백준 15724번

---
+++ src/baekjoon/p15724/Main.java
@@ -0,0 +1,43 @@
+package baekjoon.p15724;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] governor = new int[N + 1][M + 1];
+        int[][] dp = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                governor[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + governor[i][j] - dp[i - 1][j - 1];
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+        while (K-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            System.out.println(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]);
+        }
+    }
+}
