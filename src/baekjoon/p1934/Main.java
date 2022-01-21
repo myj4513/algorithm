1842ce01f949db80c85cd0d68b7bb4d42e86b37d
myj4513 <yejungod@gmail.com>
2022-01-21 23:51:01 +0900

    백준 1934번

---
+++ src/baekjoon/p1934/Main.java
@@ -0,0 +1,32 @@
+package baekjoon.p1934;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append(A / GCD(A, B) * B + "\n");
+
+        }
+        System.out.println(sb);
+    }
+
+    public static int GCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return GCD(b, a % b);
+    }
+}
