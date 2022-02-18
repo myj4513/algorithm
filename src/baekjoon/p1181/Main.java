45fe182293f4091081647585816202104f808b7c
myj4513 <yejungod@gmail.com>
2022-02-18 15:11:52 +0900

    백준 1181번

---
+++ src/baekjoon/p1181/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.p1181;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+        List<String> list = new ArrayList<>(set);
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s + "\n");
+        }
+        System.out.println(sb);
+    }
+}
