aa38d33732f2c7e1aa8d248616cfeba9992ae18d
myj4513 <yejungod@gmail.com>
2022-02-24 12:42:11 +0900

    백준 11403번

---
+++ src/baekjoon/pp11403/Main.java
@@ -0,0 +1,42 @@
+package baekjoon.pp11403;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken())==1?1:1000000;
+            }
+        }
+
+        for (int k = 0; k < N; k++)
+            for (int j = 0; j < N; j++)
+                for (int i = 0; i < N; i++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] != 1000000) {
+                    map[i][j] = 1;
+                } else {
+                    map[i][j] = 0;
+                }
+                sb.append(map[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
