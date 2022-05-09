37d28b22b9f367dbe0305d39b74fd5411a0c894f
myj4513 <yejungod@gmail.com>
2022-05-09 23:54:26 +0900

    백준 1712번

---
+++ src/baekjoon/p1712/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.p1712;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken());
+
+        if (B >= C) {
+            System.out.println(-1);
+            return;
+        }
+
+        int cnt = 1;
+        while (true) {
+            if (A + B * cnt < C * cnt) {
+                System.out.println(cnt);
+                return;
+            }
+            cnt++;
+        }
+    }
+}
