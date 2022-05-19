79685b174baf63f55973a0586e50daa148e81272
myj4513 <yejungod@gmail.com>
2022-05-19 23:02:23 +0900

    백준 9020번 unsolved

---
+++ src/baekjoon/p9020/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.p9020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+
+        while (T-- > 0) {
+
+        }
+    }
+}
