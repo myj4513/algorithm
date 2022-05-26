b08b98725ca83a65848862c13858e7a426f6a843
myj4513 <yejungod@gmail.com>
2022-05-26 22:51:26 +0900

    백준 1932번

---
+++ src/baekjoon/p1932/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.p1932;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+        int[][] dp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[N-1] = arr[N-1];
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + arr[i][j];
+            }
+        }
+
+        System.out.println(dp[0][0]);
+    }
+}
