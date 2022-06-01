9ffdc0dadc71aae74c146588e6fdfe747795d8a9
myj4513 <yejungod@gmail.com>
2022-06-01 23:12:58 +0900

    백준 10773번

---
+++ src/baekjoon/p10773/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.p10773;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        while (K-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                stack.pop();
+            } else stack.push(n);
+        }
+
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+    }
+}
