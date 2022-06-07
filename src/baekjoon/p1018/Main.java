5a0a946d77a558bb51a937944aeae62a6f189196
myj4513 <yejungod@gmail.com>
2022-06-07 22:50:37 +0900

    백준 1018번

---
+++ src/baekjoon/p1018/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.p1018;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int b = 0, w = 0;
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                if (str.charAt(j) == 'B'){ b++;}
+                else{ w++;}
+            }
+        }
+        System.out.println(b);
+        System.out.println(w);
+        System.out.println(Math.abs(b - w) / 2);
+    }
+}
