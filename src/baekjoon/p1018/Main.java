cee8bc6d3582fcbfcb0ff9e40843e40df6ff83aa
myj4513 <yejungod@gmail.com>
2022-06-13 11:57:07 +0900

    백준 1018번

--- src/baekjoon/p1018/Main.java
+++ src/baekjoon/p1018/Main.java
@@ -11,17 +11,47 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        int b = 0, w = 0;
+        int[][] map = new int[N][M];
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                if (str.charAt(j) == 'B'){ b++;}
-                else{ w++;}
+                if (str.charAt(j) == 'B') {
+                    map[i][j] = 1;
+                } else {
+                    map[i][j] = 0;
+                }
             }
         }
-        System.out.println(b);
-        System.out.println(w);
-        System.out.println(Math.abs(b - w) / 2);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N - 7; i++) {
+            for (int j = 0; j < M - 7; j++) {
+                int cnt = 0;
+                for (int x = i; x < i + 8; x++) {
+                    for (int y = j; y < j + 8; y++) {
+                        if ((x + y) % 2 == (i + j) % 2 && map[x][y] != map[i][j]) {
+                            continue;
+                        } else if ((x + y) % 2 != (i + j) % 2 && map[x][y] == map[i][j]) {
+                            continue;
+                        } else
+                            cnt++;
+                    }
+                }
+                min = Integer.min(min, cnt);
+                cnt = 0;
+                for (int x = i; x < i + 8; x++) {
+                    for (int y = j; y < j + 8; y++) {
+                        if ((x + y) % 2 == (i + j) % 2 && map[x][y] == map[i][j]) {
+                            continue;
+                        } else if ((x + y) % 2 != (i + j) % 2 && map[x][y] != map[i][j]) {
+                            continue;
+                        } else
+                            cnt++;
+                    }
+                }
+                min = Integer.min(min, cnt);
+
+            }
+        }
+        System.out.println(min);
     }
 }
