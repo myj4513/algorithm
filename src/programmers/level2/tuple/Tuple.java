d8ce71dc5dbc42e11454786671c199cc994a48f6
myj4513 <yejungod@gmail.com>
2022-06-29 23:58:37 +0900

    프로그래머스 lev.1 튜플

--- src/programmers/level2/tuple/Tuple.java
+++ src/programmers/level2/tuple/Tuple.java
@@ -1,23 +1,68 @@
 package programmers.level2.tuple;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Tuple {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] result = s.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
+        int[] result = s.solution("{{2},{2,2},{2,2,2},{2,2,2,2}}");
         System.out.println(Arrays.toString(result));
     }
 }
 
 
+/* 중복 원소가 있다는 가정의 풀이
 class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
+        s = s.substring(2, s.length() - 2);
+        String[] split = s.split("\\},\\{");
+        Arrays.sort(split, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+
+        int[] answer = new int[split.length];
+        Map<Integer, Integer> forNextMap = new HashMap<>();
+        Map<Integer, Integer> forThisMap = new HashMap<>();
+        for (int i = 0; i < split.length; i++) {
+            StringTokenizer st = new StringTokenizer(split[i], ",");
+            while (st.hasMoreTokens()) {
+                int n = Integer.parseInt(st.nextToken());
+
+                if (forThisMap.get(n) == null || forThisMap.get(n) == 0) {
+                    answer[i] = n;
+                }else{
+                    forThisMap.put(n, forThisMap.get(n) - 1);
+                }
+
+                if(forNextMap.get(n)==null){
+                    forNextMap.put(n,1);
+                } else{
+                    forNextMap.put(n, forNextMap.get(n) + 1);
+                }
+            }
+            forThisMap = forNextMap;
+            forNextMap = new HashMap<>();
+        }
+
+        return answer;
+    }
+}*/
+
+class Solution {
+    public int[] solution(String s) {
         Set<String> set = new HashSet<>();
-        System.out.println(s.substring(1,s.length()-1));
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
+            }
+        }
         return answer;
     }
 }
