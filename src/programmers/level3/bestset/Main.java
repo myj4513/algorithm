bd68e5ce158ab5d61b005076e63ef142c45fdc8e
myj4513 <yejungod@gmail.com>
2022-07-18 13:11:01 +0900

    프로그래머스 레벨 3- 최고의 집합

---
+++ src/programmers/level3/bestset/Main.java
@@ -0,0 +1,35 @@
+package programmers.level3.bestset;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().solution(2, 9)));
+    }
+}
+
+class Solution {
+    public int[] solution(int n, int s) {
+        if (s / n < 1) {
+            return new int[]{-1};
+        }
+        int[] answer = new int[n];
+        if (s % n == 0) {
+            Arrays.fill(answer, s / n);
+        }
+        int x = s / n;
+        int y = s / n + 1;
+        for (int i = 1; i < n; i++) {
+            if (x * i + y * (n - i) == s) {
+                int idx = 0;
+                for (int j = 0; j < i; j++) {
+                    answer[idx++] = x;
+                }
+                for (int j = 0; j < n - i; j++) {
+                    answer[idx++] = y;
+                }
+            }
+        }
+        return answer;
+    }
+}
