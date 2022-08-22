1e43d4c6a60905ff8d6bc80aefacba1ed6ed38da
myj4513 <yejungod@gmail.com>
2022-08-22 14:16:16 +0900

    프로그래머스 레벨 4 - 무지의 먹방 라이브 (효율성 테스트 통과)

--- src/programmers/level4/muzifoodtable/Main.java
+++ src/programmers/level4/muzifoodtable/Main.java
@@ -1,67 +1,56 @@
 package programmers.level4.muzifoodtable;
 
+/**
+ * 프로그래머스 레벨 4 - 무지의 먹방 라이브 (효율성 테스트 통과)
+ */
+
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(new int[]{3,1,1,1,2,4,3}, 12));
+        System.out.println(new Solution().solution(new int[]{3, 1, 2}, 5));
     }
 }
 
 class Solution {
     public int solution(int[] food_times, long k) {
-        long count = 0;
         List<Food> foodList = new ArrayList<>();
+        long count = 0;
         for (int i = 0; i < food_times.length; i++) {
             foodList.add(new Food(i + 1, food_times[i]));
             count += food_times[i];
         }
-        if (count < k) return -1;
-        Collections.sort(foodList);
-        while (k > foodList.get(0).size * foodList.size()) {
-            k -= (long) foodList.get(0).size * foodList.size();
-            int size = foodList.get(0).size;
-            Iterator<Food> iterator = foodList.iterator();
-            while (iterator.hasNext()) {
-                Food food = iterator.next();
-                food.size -= size;
-                if (food.size <= 0) {
-                    iterator.remove();
-                }
+        if (count <= k) return -1;
+        foodList.sort(new Comparator<Food>() {
+            @Override
+            public int compare(Food o1, Food o2) {
+                return o1.time - o2.time;
+            }
+        });
+        int idx = 0;
+        int removedTime = 0;
+        while (k > ((long) foodList.get(idx).time - removedTime) * (foodList.size() - idx)) {
+            k -= ((long) foodList.get(idx).time - removedTime) * (foodList.size() - idx);
+            removedTime = foodList.get(idx).time;
+            while (removedTime >= foodList.get(idx).time) {
+                idx++;
             }
         }
-        int idx = (int) (k % foodList.size());
-        foodList.sort((o1, o2) -> o1.idx - o2.idx);
-        return foodList.get(idx).idx;
+        List<Food> foods = foodList.subList(idx, foodList.size());
+        foods.sort((o1, o2) -> o1.idx - o2.idx);
+        k %= foods.size();
+        return foods.get((int) k).idx;
     }
 
-    class Food implements Comparable<Food> {
+    class Food {
         int idx;
-        int size;
+        int time;
 
-        public Food(int idx, int size) {
+        public Food(int idx, int time) {
             this.idx = idx;
-            this.size = size;
-        }
-
-        @Override
-        public int compareTo(Food o) {
-            if (o.size == this.size) {
-                return this.idx - o.idx;
-            }
-            return this.size - o.size;
-        }
-
-        @Override
-        public String toString() {
-            return "Food{" +
-                    "idx=" + idx +
-                    ", size=" + size +
-                    '}';
+            this.time = time;
         }
     }
 }
-
